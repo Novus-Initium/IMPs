bc491932f9e5bfd8a64b6ae04acad6ea04eb32a6
Jade Clyne <krclyne@gmail.com>
2024-06-20 21:30:13 -0600

    Added manage project page to view projects owned by user.

--- packages/nextjs/app/grants/page.tsx
+++ packages/nextjs/app/grants/page.tsx
@@ -16,7 +16,7 @@ const ProjectsPage = () => {
               Create Project
             </button>
           </Link>
-          <Link href="/projects/manage" passHref>
+          <Link href="/grants/projects/manage" passHref>
             <button className="w-full mb-6 py-3 px-6 border border-transparent rounded-lg shadow-lg text-lg font-semibold text-white bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform transition-transform duration-200 hover:-translate-y-1 active:translate-y-0">
               Manage Project
             </button>
---
+++ packages/nextjs/app/grants/projects/manage/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Head from 'next/head';
+import ProjectEvents from '../../../../components/allo/ManageProject';
+
+function CreateProjectPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <Head>
+        <title>View Projects</title>
+      </Head>
+      <div className="bg-white shadow-md rounded-lg p-8 max-w-lg w-full">
+        <ProjectEvents />
+      </div>
+    </div>
+  );
+}
+
+export default CreateProjectPage;
