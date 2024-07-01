b4ca0e68f214b9f1d1e189bf4b098b9f1dc8d74f
innkeep <innkeeping@metagame.wtf>
2024-07-01 01:30:58 +0000

    Home Page UI updates 🏠

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,88 +1,134 @@
 "use client";
 
-import { Button } from 'daisyui';
+import Head from 'next/head';
 import Link from "next/link";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
-import { BanknotesIcon, UserGroupIcon, BuildingLibraryIcon } from "@heroicons/react/24/outline";
+import { DocumentMagnifyingGlassIcon, PlusCircleIcon, BanknotesIcon, UserGroupIcon, BuildingLibraryIcon, MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-3 bg-teal-600">
+      <div className="glass flex items-center flex-col flex-grow pt-3 bg-secondary-100">
+      <Head>
+        <title>Home</title>
+      </Head>
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2 text-yellow-100">Impact Measurement Protocols</span>
-            <span className="block text-3xl font-bold">Fund A Round and Find Out</span>
+            <span className="block text-3xl mb-2 secondary-100">Impact Measurement Protocols</span>
+            <span className="block text-2xl text-secondary-200 font-bold">Fund A Round and Find Out</span>
           </h1>
         </div>
         <div className="flex-grow w-full mt-2 px-8 py-6">
           <div className="flex justify-center items-center gap-6 flex-col sm:flex-row">
-          <Link href="/projects">
-          <button className="hover:bg-base-100 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform transition-transform duration-200 hover:-translate-y-1 active:translate-y-0">
-            <div className="flex flex-col bg-base-100 px-8 py-8 text-center items-center max-w-xs rounded-3xl">
-            <h1 className="text-center">
-            <span className="block text-2xl mb-2">Projects</span>
-          </h1>
-              <UserGroupIcon className="h-8 w-8 fill-secondary" />
-                <p><b>Create</b> a mission.</p>
-                <b>Manage</b> a mission.
-                <p><b>Explore</b> live missions.{" "}</p>
-                <Link href="/projects/explore" passHref className="link">
-                  View Current Missions
-                </Link>{" "}
-                <Link href="/projects/create" passHref className="link">
-                  Start a Mission
-                </Link>{" "}
-            </div>
-            </button>
-            </Link>
-            <Link href="/grants">
-            <button className="hover:bg-base-100 hover:shadow-xl focus:outline-yellow-100 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform transition-transform duration-200 hover:-translate-y-1 active:translate-y-0">
-            <div className="flex flex-col bg-base-100 p-6 text-center items-center max-w-xs rounded-3xl">
-            <h1 className="text-center">
-            <span className="block text-2xl mb-1">Rounds</span>
-            </h1>
-            <BanknotesIcon className="h-8 w-8 fill-secondary"/>
-            <p><b>Apply</b> for a grant.</p>
-                <b>Donate</b> to a grant.
+            <div className="flex items-center card bg-base-100 w-42 hover:bg-secondary hover:shadow transform transition-transform duration-200 hover:-translate-y-1 active:translate-y-0">
+              <div className="card-body items-center">
+                <Link href="/projects" passHref className="link">
+                  <h1 className="card-title text-2xl mb-2 bold">Projects</h1>
+                </Link>
+                <UserGroupIcon className="h-8 w-8 fill-accent-content" />
                 <p>
-                <b>Attest</b> on a missions impact.
-                <p><b>Fund</b> A Round
-                <p></p><b>Find</b> Out</p>{" "}
+                  <b>Create</b> a Mission.
                 </p>
-                <Link href="/debug" passHref className="link">
-                  View Current Grants
-                </Link>{" "}
-                <Link href="/debug" passHref className="link">
-                  Donate and Vote for Impact!
-                </Link>{" "}
-            </div>
-            </button>
-            </Link>
-            <Link href="/projects">
-            <button className="hover:bg-base-100 hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transform transition-transform duration-200 hover:-translate-y-1 active:translate-y-0">
-            <div className="flex flex-col bg-base-100 px-8 py-8 text-center items-center max-w-xs rounded-3xl">
-            <h1 className="text-center">
-            <span className="block text-2xl mb-2">Manager</span>
-          </h1>
-              <BuildingLibraryIcon className="h-8 w-8 fill-secondary" />
-            <p><b>Create</b> a round.
-                </p>
-                <b>Manage</b> a round.
                 <p>
-                <b>Review</b> all donations.
-                {" "}
+                  <b>Manage</b> a Mission.
                 </p>
-                <Link href="/debug" passHref className="link">
-                  View Current Rounds
-                </Link>{" "}
-                <Link href="/debug" passHref className="link">
-                  Create a new Round
-                </Link>{" "}
+                <p>
+                  <b>Explore</b> current Projects.{" "}
+                </p>
+                <div className="card-actions">
+                  <ul className="flex flex-row menu menu-horizontal bg-base-200 rounded-box mt-6">
+                    <li>
+                      <a href="/projects/create" className="tooltip" data-tip="Create">
+                        <PlusCircleIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/projects/manage" className="tooltip" data-tip="Manage">
+                        <BuildingLibraryIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/projects/explore" className="tooltip" data-tip="Explore">
+                        <MagnifyingGlassCircleIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            </button>
-            </Link>
+            <div className="flex items-center card bg-base-100 w-42 hover:shadow-x1 hover:bg-secondary hover:shadow transform transition-transform duration-200 hover:-translate-y-1 active:translate-y-0">
+              <div className="card-body items-center">
+                <Link href="/grants" passHref className="link">
+                  <h1 className="card-title text-2xl mb-2 bold">Rounds</h1>
+                </Link>
+                <BanknotesIcon className="h-8 w-8 fill-accent-content" />
+                <p>
+                  <b>Create</b> a Grant Round.
+                </p>
+                <p>
+                  <b>Manage</b> a Round.
+                </p>
+                <p>
+                  <b>Explore</b> live Rounds.{" "}
+                </p>
+                <div className="card-actions">
+                  <ul className="menu menu-horizontal bg-base-200 rounded-box mt-6">
+                    <li>
+                      <a href="/grants/create" className="tooltip" data-tip="Create">
+                        <PlusCircleIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/grants/manage" className="tooltip" data-tip="Manage">
+                        <BuildingLibraryIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/grants/explore" className="tooltip" data-tip="Explore">
+                        <MagnifyingGlassCircleIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center card bg-base-100 w-42 hover:bg-secondary hover:shadow transform transition-transform duration-200 hover:-translate-y-1 active:translate-y-0">
+              <div className="card-body items-center">
+                <Link href="/manager" passHref className="link">
+                  <h1 className="card-title text-2xl mb-2 bold">Manage</h1>
+                </Link>
+                <BuildingLibraryIcon className="h-8 w-8 fill-accent-content" />
+                <p>
+                  <b>Manage</b> a Mission.
+                </p>
+                <p>
+                  <b>Manage</b> a Round.
+                </p>
+                <p>
+                  <b>Review</b> all Donations.{" "}
+                </p>
+                <div className="card-actions">
+                  <ul className="menu menu-horizontal bg-base-200 rounded-box mt-6">
+                    <li>
+                      <a href="/projects/manage" className="tooltip" data-tip="Project Manage">
+                        <UserGroupIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/grants/manage" className="tooltip" data-tip="Round Manage">
+                        <BanknotesIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/blockexplore"className="tooltip" data-tip="Review Donations">
+                        <DocumentMagnifyingGlassIcon className="h-8 w-8 fill-accent-content" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
