3e7a93fdc2bf08e9709c8fd8ab6e87863a798744
Jade Clyne <krclyne@gmail.com>
2024-07-28 19:42:42 -0400

    refactor: Implement client-side rendering for ExploreFutureRounds component

--- packages/nextjs/app/grants/explore/future/page.tsx
+++ packages/nextjs/app/grants/explore/future/page.tsx
@@ -1,5 +1,8 @@
+"use client"
 import React from 'react';
-import ExploreFutureRounds from '../../../../components/allo/ExploreFutureRounds';
+import dynamic from 'next/dynamic';
+
+const ExploreFutureRounds = dynamic(() => import('../../../../components/allo/ExploreFutureRounds'), { ssr: false });
 
 const ExploreFutureRoundsPage = () => {
   return (
